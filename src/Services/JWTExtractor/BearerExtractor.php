2121091f328463f3c0a64eaeadfb370372977b2c
Louis-Philippe Frenette <lp@marketingauto.ca>
2025-01-29 08:38:30 -0500

    Fix token extraction logic and update .gitignore
    Adjust the BearerExtractor to handle case-insensitive 'Bearer' tokens and prevent errors with empty authorization headers. Additionally, update .gitignore to include .idea files for development environments.

--- src/Services/JWTExtractor/BearerExtractor.php
+++ src/Services/JWTExtractor/BearerExtractor.php
@@ -9,27 +9,27 @@ use Symfony\Component\HttpFoundation\Request;
 class BearerExtractor implements JWTExtractorInterface
 {
 
-	public function supports(Request $request): bool
-	{
-		return $request->headers->has('Authorization');
-	}
-	
-	/**
-	 * @inheritdoc
-	 * According to the specification, the token is parsed
-	 * assuming that exactly one whitespace character separates
-	 * the authentication scheme ('Bearer') and the token.
-	 * @link https://datatracker.ietf.org/doc/html/rfc6750#section-2.1
-	 */
-	public function extract(Request $request): string
-	{
-		$authorizationHeader = $request->headers->get('Authorization');
-		$headerParts = explode(' ', $authorizationHeader, 3);
+    public function supports(Request $request): bool
+    {
+        return $request->headers->has('Authorization');
+    }
 
-		if (count($headerParts) !== 2 || $headerParts[0] !== 'Bearer') {
-			throw new InvalidArgumentException('Invalid authorization header!');
-		}
+    /**
+     * @inheritdoc
+     * According to the specification, the token is parsed
+     * assuming that exactly one whitespace character separates
+     * the authentication scheme ('Bearer') and the token.
+     * @link https://datatracker.ietf.org/doc/html/rfc6750#section-2.1
+     */
+    public function extract(Request $request): string
+    {
+        $authorizationHeader = $request->headers->get('Authorization');
+        $headerParts = explode(' ', $authorizationHeader, 3);
 
-		return $headerParts[1];
-	}
+        if (count($headerParts) !== 2 || empty($headerParts) || strtoupper($headerParts[0]) !== 'BEARER') {
+            throw new InvalidArgumentException('Invalid authorization header!');
+        }
+
+        return $headerParts[1];
+    }
 }
